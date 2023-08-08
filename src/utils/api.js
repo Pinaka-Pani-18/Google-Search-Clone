2000581ba982828ea6668e1fe73a97595b69c997
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-08-08 22:36:16 +0530

    data is fetched from google custom api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,18 @@
 import axios from "axios";
 
-export const fetchDataFromApi = () => {};
+const BASE_URL = "https://www.googleapis.com/customsearch/v1";
+
+const params = {
+  key: "AIzaSyBAmg-zExNAUgQ2iTkpTDH3hLRNBQVWNss",
+  cx: "9793a0cceab8c4b9e",
+};
+
+export const fetchDataFromApi = async (payload) => {
+  const { data } = await axios.get(BASE_URL, {
+    params: {
+      ...params,
+      ...payload,
+    },
+  });
+  return data;
+};
